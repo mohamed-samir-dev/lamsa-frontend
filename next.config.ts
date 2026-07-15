b551e93a45dfaffc79abe22dbe77e57a9db6ef28
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-07-15 19:26:02 +0300

    Activating the brute force protection using loginAttempts and lockUntil fields in the login logic

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,6 @@ const nextConfig: NextConfig = {
     };
   },
   images: {
-    qualities: [60, 75, 100],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
